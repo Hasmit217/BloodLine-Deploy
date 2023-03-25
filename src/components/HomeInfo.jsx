e72d6434a771cdb859564b2bff27fe91a2a6c551
Ekansh Lohiya <ekanshlohiya98@gmail.com>
2023-03-25 19:28:22 +0530

    minor tweaks

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -7,6 +7,7 @@ function HomeInfo(){
             <h2>Know about Donation</h2>
             <img className = "dyk_img" src="images/home_dyk.png"></img>
             <img className = "bc_img" src="images/blood_compatibility.jpeg" alt = "blood compatible table"></img>
+            <h2>Benefits</h2>
             <img className="benefits_img" src="images/benefits.png"></img>
         </div>
     )
