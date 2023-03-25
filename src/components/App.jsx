00f6a2f7f2ce0750e3233f069d23c26a1e00cb67
hasmit chauhan <hasmitchauhan99@gmail.com>
2023-03-26 04:14:54 +0530

    js file organized

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,15 @@
 import React from "react";
-import HomeCard from "./HomeCard";
-import HomeInfo from "./HomeInfo";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Footer from "./layout/Footer";
+import Navbar from "./layout/Navbar";
+import Home from "./dashboard/Home"
 
 function App(){
     return(
         <div>
             <Navbar></Navbar>
-            <div className="row">
-                <div className="column card1"><HomeCard pageURL = "#" name = "Donor Login/Register"  className="HomeCard"></HomeCard></div>
-                <div className="column card2"><HomeCard pageURL = "#" name = "Blood Camp Registration" className="HomeCard"></HomeCard></div>
-                <div className="column card3"><HomeCard pageURL = "#" name = "Blood Availability Search" className="HomeCard"></HomeCard></div>
-                <div className="column card4"><HomeCard pageURL = "#" name = "Blood Bank Directory" className="HomeCard"></HomeCard></div>
-                <div className="column card5"><HomeCard pageURL = "#" name = "Blood Donation Camps" className="HomeCard"></HomeCard></div>
-            </div>
-
-            <HomeInfo></HomeInfo>
+            
+            <Home></Home>
+            
             <Footer></Footer>
         </div>
     )
