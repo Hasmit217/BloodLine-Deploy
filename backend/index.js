e3900a77555fe51fd95faa0027166ab25de950a2
Ekansh Lohiya <ekanshlohiya98@gmail.com>
2023-04-08 22:08:39 +0530

    bloodbanks database done

--- backend/index.js
+++ backend/index.js
@@ -4,25 +4,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-const BloodRequest = require("./models/bloodReq");
-
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//================ routes and models ========================
 
+const BloodRequest = require("./models/bloodReq");
 const bloodReqRoute = require("./routes/bloodReq");
+
+const BloodBank = require("./models/bloodBanks");
+const BloodBankRoute = require("./routes/bloodBanks");
+
+//===========================================================
 
 
 mongoose.connect("mongodb+srv://ekanshlohiya98:Bloodline%40she98@cluster0.9k4kzet.mongodb.net/bloodlineDB");
 
 app.use("/",bloodReqRoute);
+app.use("/",BloodBankRoute);
 
 app.listen(8080, function() {
     console.log("Server started on port 8080");
