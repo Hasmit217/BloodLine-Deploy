e3abe69398dadba909886e2954fa0aa1e7aa96e8
Hasmit <hasmitchauhan99@gmail.com>
2024-06-03 14:01:21 +0530

    disable ESlint

--- src/components/bloodBank/BBGuildlines.jsx
+++ src/components/bloodBank/BBGuildlines.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BBGuildlines = () => {
     return (
         <div class="bb-guid-container">
-            <img class = "bb-guid-pic" src = "images/red-blood-cells.jpg"></img>
+            <img class = "bb-guid-pic" src = "images/red-blood-cells.jpg" alt="guidlines"></img>
         </div>
     )
 };
